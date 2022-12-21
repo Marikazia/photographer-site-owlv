de6774eff7337ddeabd371196aa82735136d0c22
Mariana Machohan <debrisofme9@gmail.com>
2022-12-21 23:31:30 +0100

    f

--- src/components/elements/Header/Nav.jsx
+++ src/components/elements/Header/Nav.jsx
@@ -2,24 +2,26 @@ import React from "react"
 
 const Nav = () => {
 	const navs = [
-		{ id: 1, name: "/" },
-		{ id: 2, name: "/" },
-		{ id: 3, name: "/" },
-		{ id: 4, name: "/" },
+		{ id: 1, name: "Home" },
+		{ id: 2, name: "About me" },
+		{ id: 3, name: "Shop" },
+		{ id: 4, name: "Blog" },
+		{ id: 5, name: "Contacts" }
 	];
 
 	return (
-		<div className="nav-top">
-			<ul>
-				{this.state.navs.map(() =>
-					<li className="nav-name" key={navs.name}>
-						{navs.name}
-					</li>
-				)}
-			</ul>
-		</div>
-	)
-
+		<>
+			<div className="nav-top">
+				<ul>
+					{navs.map(() =>
+						<li className="nav-name" key={navs.name}>
+							{navs.name}
+						</li>
+					)}
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default Nav;
